83b43f3db85748f1b2d722088e5227b200ccc485
youmi <128112408@qq.com>
2022-09-30 17:26:42 +0800

    增加了单例类

--- youmitools.py
+++ youmitools.py
@@ -121,6 +121,22 @@ def gen_step(num: int, n: int) -> list:
         return lst_com
 
 
+class Singleton(type):
+    # new用于创建类对象
+    def __new__(cls, class_name, class_parents, class_attr):
+        # 给类添加一个instance属性
+        class_attr['instance'] = None
+
+        # 返回类对象
+        return super(Singleton, cls).__new__(cls, class_name, class_parents, class_attr)
+
+    # 类对象实例化时将调用
+    def __call__(self, *args, **kwargs):
+        if self.instance is None:
+            self.instance = super(Singleton, self).__call__(*args, **kwargs)
+        return self.instance
+
+
 if __name__ == '__main__':
     def gen_step_Test():
         """
@@ -199,6 +215,17 @@ if __name__ == '__main__':
             #     [1, 0]
             #     [2, 0]
 
+
+    def sigle_test():
+        class Test(metaclass=Singleton):
+            def __init__(self, id):
+                self.id = id
+
+        t = Test(1)
+        t1 = Test(2)
+        print(t.id)     # 1
+        print(t1.id)    # 1
+        print(t is t1)
 
 
 
